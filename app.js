c97e54a27df40a077e1708c4d32b0842cd4680ef
Azeezullah <azeezullahnoohpoto98@gmail.com>
2024-05-04 22:20:14 +0500

    This is my Word-Counter.Project

---
+++ app.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log("=".repeat(50));
+console.log(chalk.bold.yellow("\n \t\t AZ Word-Counter"));
+console.log("=".repeat(50));
+let answers = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter a Sentence",
+    }
+]);
+let words = answers.Sentence.trim().split(" ");
+console.log("=".repeat(50));
+console.log(chalk.bold("- Sentence Words:"));
+console.log(words);
+console.log(chalk.bold(`\n - word Count: ${chalk.bold.redBright(words.length)}`));
+console.log("=".repeat(50));
